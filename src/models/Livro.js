af5b258249cbc65631d10d8dae8c1924c2dfe8af
Luiz Bocaiuva <luizhsbocaiuva@gmail.com>
2026-02-13 16:59:55 -0300

    feat: crud completo com paginação e busca avançada

---
+++ src/models/Livro.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import autopopulate from "mongoose-autopopulate";
+
+const livroSchema = new mongoose.Schema(
+  {
+    id: {type: String},
+    titulo: {
+      type: String, 
+      required: [true, 'O título do livro é obrigatório!']
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'autores', 
+      required: [true, 'O autor(a) é obrigatório!'],
+      autopopulate: { select: "nome" }
+    },
+    editora: {
+      type: String, 
+      required: [true, 'A editora é obrigatório!'],
+      enum: {
+        values: ['Casa do Código', 'Alura'],
+        message: 'A editora {VALUE} não é uma editora permitida'
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+      },
+      message: 'O número de páginas deve estar entre 10 e 5000! Valor fornecido {VALUE}'
+      }
+    }
+  }
+);
+
+livroSchema.plugin(autopopulate);
+const livros= mongoose.model('livros', livroSchema);
+
+export default livros;
